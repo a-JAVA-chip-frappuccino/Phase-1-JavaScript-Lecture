0c4fc32892af13141a6b45641dc3ce63ced688f8
Eleanor Kelman <ekelman3@gmail.com>
2023-05-05 16:51:45 -0400

    commented POST request lecture

--- client/src/POST-requests.js
+++ client/src/POST-requests.js
@@ -51,28 +51,30 @@ const form = document.querySelector('form')
 function handleSubmit(e) {
     e.preventDefault()
 
+    // grabs entered form values
     let id = document.querySelector('#id').value
     let title = e.target.title.value
     let author = document.querySelector('input#author').value
     let imageURL = document.getElementById('image').value
     let pages = e.target.pages.value
 
-    fetch("http://localhost:3000/books", {
+    // POST request
+    fetch("http://localhost:3000/books", { // routes to API
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(
-            {
+        body: JSON.stringify( // converts object to JSON
+            { // creates object to POST to API
                 // "id" : id,
                 "title" : title,
                 "author" : author,
                 "image" : imageURL,
-                "pages" : parseInt(pages)
+                "pages" : parseInt(pages) // typecasts to integer
             }
         )
     })
 }
 
-form.addEventListener('submit', handleSubmit)
+form.addEventListener('submit', handleSubmit) // POSTS on form submission
