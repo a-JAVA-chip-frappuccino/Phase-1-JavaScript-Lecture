c110bfb3a032de8d98e690231d87e2201c3e64b0
Eleanor Kelman <ekelman3@gmail.com>
2023-05-05 15:35:13 -0400

    completed POST request lecture

---
+++ client/src/POST-requests.js
@@ -0,0 +1,78 @@
+// JavaScript code goes here
+
+// commands to run db.json server
+// npm install -g json-server
+// json-server --watch db.json
+
+/*
+function postMessage(id, title, author, imageURL, pages) {
+
+    fetch("http://localhost:3000/books", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                "id" : parseInt(id),
+                "title" : title,
+                "author" : author,
+                "image" : imageURL,
+                "pages" : parseInt(pages)
+            }
+        )
+    })
+    // .then((resp) => resp.json())
+    // .then((data) => console.log(data))
+}
+
+const form = document.querySelector('form')
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    let id = document.querySelector('input#id').value
+    let title = document.getElementById('title').value
+    // let title = document.querySelector("#title").value
+    let author = e.target.author.value
+    let imageURL = e.target.image.value
+    let pages = e.target.pages.value
+
+    postMessage(id, title, author, imageURL, pages)
+
+}
+
+form.addEventListener('submit', handleSubmit)
+*/
+
+const form = document.querySelector('form')
+
+function handleSubmit(e) {
+    e.preventDefault()
+
+    let id = document.querySelector('#id').value
+    let title = e.target.title.value
+    let author = document.querySelector('input#author').value
+    let imageURL = document.getElementById('image').value
+    let pages = e.target.pages.value
+
+    fetch("http://localhost:3000/books", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                // "id" : id,
+                "title" : title,
+                "author" : author,
+                "image" : imageURL,
+                "pages" : parseInt(pages)
+            }
+        )
+    })
+}
+
+form.addEventListener('submit', handleSubmit)
